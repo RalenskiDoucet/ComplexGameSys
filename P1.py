e9cfcf582d6f3eb9fe21a3981a51b2e6f0f8854d
Ralenski Doucet <Ralenskid@gmail.com>
2019-01-08 17:56:55 -0600

    Initial commit

---
+++ P1.py
@@ -0,0 +1,17 @@
+class CNFSatisfiablity():
+    def init_(self,filename),
+        self.strings=[]
+        self.symbols(!,&,|)
+        self.variables = (a,b,c,d,e,f)
+    a = 1
+    b = 0
+    c = 1
+    d = 0
+    e = 1
+    f = 0
+    ! = not
+    | = or
+    & = and
+    def openfile()
+        Readme = open ('CNFSatisfiablity.txt','r',).read()
+        print(Readme)
