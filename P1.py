14f7a0a0aed5fbe207dfd4034e7a8b87caa4f72f
Ralenski Doucet <Ralenskid@gmail.com>
2019-01-09 12:56:20 -0600

    commit still not able to read from file

--- P1.py
+++ P1.py
@@ -1,17 +1,21 @@
+#DI
 class CNFSatisfiablity():
-    def init_(self,filename),
-        self.strings=[]
-        self.symbols(!,&,|)
+    def init_(self,variables,symbols):
         self.variables = (a,b,c,d,e,f)
-    a = 1
-    b = 0
-    c = 1
-    d = 0
-    e = 1
-    f = 0
-    ! = not
-    | = or
-    & = and
-    def openfile()
-        Readme = open ('CNFSatisfiablity.txt','r',).read()
-        print(Readme)
+        a = 1
+        b = 0
+        c = 1
+        d = 0
+        e = 1
+        f = 0
+class Paser():
+    def init_(self,filename,strings):
+        self.file = filename
+        self.strings=[]
+    def parse_(self,filename)
+#open and read from file convert all lines of text from the file to a array of strings
+        filename = open("CNFSatisfiablity",'r')
+        strings = filename.readlines()
+        filename.close()
+        print(strings)
+
