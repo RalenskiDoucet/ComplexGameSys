f78da90e662466838b79817b888b3b1e8fc768c6
Ralenski Doucet <Ralenskid@gmail.com>
2019-01-10 13:49:48 -0600

    cmmit

--- P1.py
+++ P1.py
@@ -1,24 +1,29 @@
-class CNFSatisfiablity(object):
+#this class will be used for holding the symbols and other things for the expression.
+class CNFSatisfiablityExpression():
     def init_(self,expression):
-        self.variables = ["a","b","c","d","e","f"]
-        self.symbols =["!","|","&"]
-        self.values = [1,1,1,1,1,1]
         self.literals = []
+        self.values = [1,1,1,1,1,1]
+        self.symbols =["!","|","+","*"]
+        self.clauses = expression.split("*")
+
         for character in expression:
             if character not in self.symbols:
                 self.literals.append(character)
                 self.literals = list(set(self.literals))
                 self.literals.sort()
         for character in self.literals:
-        for value in self.values:
-             character = value
+            for value in self.values:
+                character = value
         print(self.literals)
-class Paser(object):
+
+#this class will read from the file.
+class Paser():
     def init_(self,filename):
         self.file = filename
         self.file_to_open = open(self.file,"r")
-        self.lines = file_to_open.readlines()
-a=Paser("CNFSatisfiablity.txt")
-for line in a.lines
-    j=expression(line)
+        self.lines = self.file_to_open.readlines()
+
+n = Paser("CNFSatisfiablity.txt")
+for line in n.lines:
+    j = CNFSatisfiablityExpression(line)
 
